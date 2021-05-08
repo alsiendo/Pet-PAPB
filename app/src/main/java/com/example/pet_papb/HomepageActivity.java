c10029fd93c0453ff98e42109ef0f4ad078c0621
alsiendo <alsiendodewantara@gmail.com>
2021-05-08 21:02:39 +0700

    Membenarkan posisi nav dan default fragmentnya

--- app/src/main/java/com/example/pet_papb/HomepageActivity.java
+++ app/src/main/java/com/example/pet_papb/HomepageActivity.java
@@ -18,6 +18,8 @@ public class HomepageActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
