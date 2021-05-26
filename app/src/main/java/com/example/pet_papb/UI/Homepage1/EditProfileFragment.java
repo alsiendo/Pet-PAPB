42c331b008820e709080612b3b9bb9563c49997d
Arfian Arya <arfianarya@gmail.com>
2021-05-27 04:42:44 +0800

    Fitur Profile : - Sudah bisa menampilkan profile - Update dan change password belum bisa

---
+++ app/src/main/java/com/example/pet_papb/UI/Homepage1/EditProfileFragment.java
@@ -0,0 +1,28 @@
+package com.example.pet_papb.UI.Homepage1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pet_papb.R;
+
+public class EditProfileFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    }
+}
